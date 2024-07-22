4037d09f93b1cb7c09dc34dfbc21ae723c1aab1a
Christen <christen.foad@gmail.com>
2024-07-22 12:29:14 +0300

    Day4 Update

---
+++ DotNetQ3/Controllers/StateController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetQ3.Controllers
+{
+    public class StateController : Controller
+    {
+        public IActionResult SetSession(string name)
+        {
+            //logic
+            //Convert object to json 
+            HttpContext.Session.SetString("Name",name);
+            HttpContext.Session.SetInt32("Age", 12);
+            //logic
+            return Content("Session Data Saved");
+        }
+
+        public IActionResult GetSession()
+        {
+            //logic
+            string n=HttpContext.Session.GetString("Name");
+            int a = HttpContext.Session.GetInt32("Age").Value;
+            //loic
+            return Content($"name={n}\t age={a}");
+
+        }
+
+
+        public IActionResult setCookie()
+        {
+            //logic
+            //state Resourec
+            //Type Of Cookie 
+            //Session Cookie 
+            HttpContext.Response.Cookies.Append("Name", "Ahmed");
+            //Presisitent cookie
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(1); ;
+            HttpContext.Response.Cookies.Append("Age", "12", options);
+            return Content("Cookie SAve");
+
+        }
+        public IActionResult GetCookie()
+        {
+            //server need from client
+            string n=HttpContext.Request.Cookies["Name"];
+            string a = HttpContext.Request.Cookies["Age"];
+            //logic
+            return Content($"name={n} \t age={a}");
+
+        }
+    }
+}
--- DotNetQ3/Program.cs
+++ DotNetQ3/Program.cs
@@ -9,14 +9,50 @@ namespace DotNetQ3
             // Add services to the container.//Service s Day8  
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+            
             var app = builder.Build();
+            
+            #region Custom (inline ) Middleware
 
+            //app.Use(async (httpContext, next)=>{
+            //    await httpContext.Response.WriteAsync("1) Middleware 1\n");
+            //    await next.Invoke();
+            //    await httpContext.Response.WriteAsync("1) Middleware 1--\n");
+
+            //});
+            //app.Use(async (httpContext, next) => {
+            //    await httpContext.Response.WriteAsync("2) Middleware 2\n");
+            //    await next.Invoke();
+            //    await httpContext.Response.WriteAsync("2) Middleware 2--\n");
+
+            //});
+            //app.Run(async (httpContext) =>
+            //{
+            //    await httpContext.Response.WriteAsync("3) Terminate\n");
+
+            //});
+
+            //app.Use(async (httpContext, next) => {
+            //    await httpContext.Response.WriteAsync("4) Middleware 4\n");
+            //    await next.Invoke();
+            //    await httpContext.Response.WriteAsync("4) Middleware 4--\n");
+
+            //});
+            #endregion
+            #region Build In Middleware
+           
             // Configure the HTTP request pipeline.//Middleawre Day 2/3
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();
+            app.UseStaticFiles();//request ==>redirect wwwroot
+
+            app.UseSession();/*session default setting*/
 
             app.UseRouting();
 
@@ -25,8 +61,16 @@ namespace DotNetQ3
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
+            #endregion
             app.Run();
         }
     }
 }
+/*
+ builder
+setting
+build
+Middleware
+ 
+ 
+ */
